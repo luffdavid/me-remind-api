f5e1b8ccdbae00d3d741a4753821204a453b12f2
luffdavid <luffd@mediamarktsaturn.de>
2023-08-29 11:02:28 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express()
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+  });
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(
+"mongodb+srv://ADMIN:ADMIN@reminder-cluster.copnp48.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to DB"))
+.catch(console.error);
+
+//import Todo SCHEMA 
+//Todo = Todo model
+//todos = all todos
+//creating a new Todo = todo
+const Todo = require('./models/Todo');
+
+app.get('/todos', async(req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+      } catch (error) {
+        next(error); // Weiterleitung an Fehlerbehandlung
+      }
+  });
+
+app.post('/todo/new', async (req, res, next) => {
+   try {
+    const todo = new Todo({
+        text : req.body.text
+    });
+   await todo.save();
+   res.json(todo);
+} catch(error) {
+    next(error);
+}
+});
+
+app.delete('/todo/delete/:id', async (req, res, next) => {
+    try {
+      const result = await Todo.findByIdAndDelete(req.params.id);
+      res.json({ result });
+    } catch (error) {
+      next(error); // Weiterleitung an Fehlerbehandlung
+    }
+});
+
+app.get('/todo/complete/:id', async (req, res) => {
+    try {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.complete = !todo.complete;
+
+	todo.save();
+
+	res.json(todo);
+} catch (error) {
+    next(error); // Weiterleitung an Fehlerbehandlung
+  }
+})
+
+app.put('/todo/update/:id', async (req, res) => {
+	try {
+    const todo = await Todo.findById(req.params.id);
+
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
+    } catch (error){
+        next(error)
+    }
+});
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found' });
+  });
+
+app.listen(3001, () => console.log("Server running on port 3001"));
